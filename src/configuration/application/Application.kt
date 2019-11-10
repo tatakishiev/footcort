e43cde106c00ef698107597e593619f94e325014
Abdukarimov Timur <abdukarimov.tim@chainsource.org>
2019-11-10 06:53:18 +0600

    Added properties reading

--- src/configuration/application/Application.kt
+++ src/configuration/application/Application.kt
@@ -1,21 +1,20 @@
 package configuration.application
 
+import com.typesafe.config.ConfigFactory
 import configuration.database.DatabaseFactory
 import configuration.kodein.KodeinModule
 import controller.court.court
 import endpoint.court.CourtEndpointImpl
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.config.HoconApplicationConfig
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.StatusPages
 import io.ktor.jackson.jackson
 import io.ktor.routing.Routing
-import io.ktor.server.engine.BaseApplicationEngine
-import io.ktor.server.engine.EngineAPI
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 import org.kodein.di.generic.instance
-
 
 @EngineAPI
 fun setup(): BaseApplicationEngine {
@@ -24,25 +23,39 @@ fun setup(): BaseApplicationEngine {
 }
 
 @EngineAPI
-fun configureServer(
-): BaseApplicationEngine {
+fun configureServer(): BaseApplicationEngine {
+
+    val port: Int = HoconApplicationConfig(ConfigFactory.load()).property("ktor.deployment.port").getString().toInt()
+    val host: String = HoconApplicationConfig(ConfigFactory.load()).property("ktor.deployment.host").getString()
+
+    val env = applicationEngineEnvironment {
+        connector {
+            this.host = host
+            this.port = port
+        }
+        module {
+            routingModule()
+            mainDependencies()
+        }
+    }
+
     return embeddedServer(
-        Netty,
-        port = 8080,
-        module = Application::mainModule,
-        watchPaths = listOf("mainModule")
+        factory = Netty,
+        environment = env
     )
 }
 
-fun Application.mainModule() {
+fun Application.routingModule() {
     val courtEndpoint by KodeinModule.kodein.instance<CourtEndpointImpl>()
-    install(ContentNegotiation) {
-        jackson { }
-    }
-
-    install(StatusPages)
     install(Routing) {
         court(courtEndpoint)
     }
 }
 
+
+fun Application.mainDependencies() {
+    install(ContentNegotiation) {
+        jackson { }
+    }
+    install(StatusPages)
+}
